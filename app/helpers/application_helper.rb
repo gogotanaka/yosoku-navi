794a1551aba1be7bdeeedd7bb377faf94c1c1aa4
gogotanaka <yakuruto771@live.jp>
2013-11-11 03:09:22 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
 	def compare(previous, current)
-		diff = previous.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i - current.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i
-		diffcomma = diff.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
-		if diff > 0
-			diffcomma = "+" + diffcomma
-			return "<p class='plus'>#{diffcomma}</p>".html_safe
-		else
-			return "<p class='minus'>#{diffcomma}</p>".html_safe
+		if previous || current
+			diff = previous.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i - current.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i
+			diffcomma = diff.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+			if diff > 0
+				diffcomma = "+" + diffcomma
+				return "<span class='up'>#{diffcomma}</span>".html_safe
+			else
+				return "<span class='down'>#{diffcomma}</span>".html_safe
+			end
 		end
 	end
 
