98599ec96fb0b96a6cc7c09ce033174ede9bccd5
gogotanaka <yakuruto771@live.jp>
2013-11-11 03:14:07 +0900

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def compare(previous, current)
-		if previous || current
+		if previous && current
 			diff = previous.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i - current.gsub(/(\d{0,3}),(\d{3})/, '\1\2').to_i
 			diffcomma = diff.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
 			if diff > 0
