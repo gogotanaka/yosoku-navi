c5e12e867c8e1b4d06b89326d988b9cb526304f7
gogotanaka <yakuruto771@live.jp>
2013-11-18 02:48:49 +0900

    fix

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,4 @@
 class Stock < ActiveRecord::Base
-  attr_accessible :chart, :code, :high, :low, :market, :name, :opening, :previousprice, :price, :volume
+  attr_accessible :chart, :code, :high, :low, :market, :name, :opening, :previousprice, :price, :volume, :previousvolume, :industry, :margin_buying, :margin_selling, :d_margin_buying, :d_margin_selling
   has_many :comments
 end
